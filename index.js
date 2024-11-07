c576c41db50996d53e2425e4418adce032ec079b
joshuaWithey <83647960+joshuaWithey@users.noreply.github.com>
2024-11-07 21:48:22 +1100

    Push

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Set canvas to window size
 let canvas = document.getElementById("myCanvas");
-// const canvas = DOM.canvas(500, 500);
 const windowHeight = window.innerHeight;
 
 // Render width of the canvas
 const gridWidth = 1000;
 const aspectRatio = window.innerHeight / window.innerWidth;
+
+let mouseDownX;
+let mouseDownY;
+let mouseDown = false;
 
 const gridHeight = Math.round(gridWidth * aspectRatio);
 canvas.setAttribute("width", gridWidth);
@@ -17,7 +20,7 @@ const inteval = 1;
 const maxIteration = 1000;
 const zoomFactor = 10;
 
-// Graph ranges
+// Initial graph ranges
 let xMin = -2;
 let xMax = 0.47;
 let yMin = -1.12;
@@ -42,11 +45,6 @@ if (gridWidth > gridHeight) {
   yMax += padding;
 }
 
-const context = canvas.getContext("2d");
-
-document.getElementById("myCanvas").addEventListener("contextmenu", zoomOut);
-document.getElementById("myCanvas").addEventListener("wheel", scrollEvent);
-
 const xArray = [];
 const yArray = [];
 for (let i = 0; i < gridWidth; i++) {
@@ -60,10 +58,8 @@ const gpu = new window.GPU.GPU({
   canvas,
   context: gl,
 });
-// gl.enable(gl.BLEND);
-// gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 const createCanvas = gpu.createKernel(
-  function (xArray, yArray, xMin, xMax, yMin, yMax, canvas) {
+  function (xArray, yArray, xMin, xMax, yMin, yMax) {
     const oldX =
       (xArray[this.thread.x] * (xMax - xMin)) / (this.constants.gridWidth - 1) +
       xMin;
@@ -87,33 +83,12 @@ const createCanvas = gpu.createKernel(
       x = tempX;
       iteration++;
     }
-    // this.constants.context.fillStyle = "black";
-    // this.constants.context.fillRect(i, gridHeight - j, 1, 1);
-    // return iteration / this.constants.maxIteration;
     if (breakBounds) {
-      const brightness = iteration / this.constants.maxIteration;
-      // const brightness = convertRange2(
-      //   iteration,
-      //   0,
-      //   0,
-      //   this.constants.maxIteration,
-      //   1
-      // );
-      // return [0, 0, 0, 1];
+      const brightness = Math.sqrt(iteration / this.constants.maxIteration);
       this.color(0, 0, 1, brightness);
-      // const brightness = scale(iteration);
-      // console.log(brightness);
-      // context.fillStyle = `rgb(0,0,255,${brightness})`;
-      // context.fillStyle = "red";
-      // context.fillRect(i, gridHeight - j, 1, 1);
     } else {
-      // return [255, 255, 255, 1];
       this.color(0, 0, 0, 1);
-      // context.fillStyle = "black";
-      // context.fillRect(i, gridHeight - j, 1, 1);
     }
-    // this.color(0, 0, 0, 1);
-    // return oldX;
   },
   {
     output: [gridWidth, gridHeight],
@@ -123,38 +98,26 @@ const createCanvas = gpu.createKernel(
       maxIteration: maxIteration,
     },
     functions: {
-      convertRange2,
+      convertRange,
     },
     graphical: true,
   }
 );
-function drawGraph2() {
-  // context.clearRect(0, 0, canvas.width, canvas.height);
-  const colors = createCanvas(xArray, yArray, xMin, xMax, yMin, yMax, canvas);
-  const canvas2 = createCanvas.canvas;
-  canvas2.id = "myCanvas";
-  canvas2.style = style = "width: 100%";
-  canvas.parentNode.replaceChild(canvas2, canvas);
+function drawGraph() {
+  createCanvas(xArray, yArray, xMin, xMax, yMin, yMax);
   canvas = document.getElementById("myCanvas");
-  document.getElementsByTagName("body")[0].appendChild(canvas2);
-  // Update event listeners
-  document.getElementById("myCanvas").addEventListener("contextmenu", zoomOut);
-  document.getElementById("myCanvas").addEventListener("wheel", scrollEvent);
-  document.getElementById("myCanvas").addEventListener("mousemove", mousePan);
-  document.getElementById("myCanvas").addEventListener("mouseup", mouseUp);
-  document
-    .getElementById("myCanvas")
-    .addEventListener("mousedown", mouseDownEvent);
-}
+  const newCanvas = createCanvas.canvas;
+  newCanvas.id = "myCanvas";
+  newCanvas.style = "width: 100%";
+  canvas.parentNode.replaceChild(newCanvas, canvas);
 
-let mouseDownX;
-let mouseDownY;
-let mouseDown = false;
-document
-  .getElementById("myCanvas")
-  .addEventListener("mousedown", mouseDownEvent);
-document.getElementById("myCanvas").addEventListener("mouseup", mouseUp);
-document.getElementById("myCanvas").addEventListener("mousemove", mousePan);
+  // Update event listeners
+  newCanvas.addEventListener("wheel", scrollEvent);
+  newCanvas.addEventListener("mousemove", mousePan);
+  newCanvas.addEventListener("mouseup", mouseUp);
+  newCanvas.addEventListener("mousedown", mouseDownEvent);
+}
+drawGraph();
 
 function mouseDownEvent(event) {
   event.preventDefault();
@@ -163,11 +126,11 @@ function mouseDownEvent(event) {
   }
   mouseDown = true;
   // Convert mouse coords from scaled canvas to actual canvas width
-  mouseDownX = convertRange2(event.offsetX, 0, 0, scaledGridWidth, gridWidth);
-  mouseDownY = convertRange2(event.offsetY, 0, 0, scaledGridHeight, gridHeight);
+  mouseDownX = convertRange(event.offsetX, 0, 0, scaledGridWidth, gridWidth);
+  mouseDownY = convertRange(event.offsetY, 0, 0, scaledGridHeight, gridHeight);
   // Convert actual canvas coords into mandelbrot space coords
-  mouseDownX = convertRange2(mouseDownX, 0, xMin, gridWidth, xMax);
-  mouseDownY = convertRange2(mouseDownY, 0, yMax, gridHeight, yMin);
+  mouseDownX = convertRange(mouseDownX, 0, xMin, gridWidth, xMax);
+  mouseDownY = convertRange(mouseDownY, 0, yMax, gridHeight, yMin);
 }
 
 function mouseUp(event) {
@@ -181,18 +144,30 @@ function mouseUp(event) {
 function mousePan(event) {
   if (mouseDown) {
     // Convert mouse coords from scaled canvas to actual canvas width
-    mouseUpX = convertRange2(event.offsetX, 0, 0, scaledGridWidth, gridWidth);
-    mouseUpY = convertRange2(event.offsetY, 0, 0, scaledGridHeight, gridHeight);
+    let mouseUpX = convertRange(
+      event.offsetX,
+      0,
+      0,
+      scaledGridWidth,
+      gridWidth
+    );
+    let mouseUpY = convertRange(
+      event.offsetY,
+      0,
+      0,
+      scaledGridHeight,
+      gridHeight
+    );
     // Convert actual canvas coords into mandelbrot space coords
-    mouseUpX = convertRange2(mouseUpX, 0, xMin, gridWidth, xMax);
-    mouseUpY = convertRange2(mouseUpY, 0, yMax, gridHeight, yMin);
+    mouseUpX = convertRange(mouseUpX, 0, xMin, gridWidth, xMax);
+    mouseUpY = convertRange(mouseUpY, 0, yMax, gridHeight, yMin);
     const xDifference = mouseDownX - mouseUpX;
     const yDifference = mouseDownY - mouseUpY;
     xMin += xDifference;
     xMax += xDifference;
     yMin += yDifference;
     yMax += yDifference;
-    drawGraph2();
+    drawGraph();
   }
 }
 
@@ -205,30 +180,22 @@ function scrollEvent(event) {
   }
 }
 
-drawGraph2();
-
 function zoomIn(event) {
   event.preventDefault();
   const xCentre = event.offsetX;
   const yCentre = event.offsetY;
   // Convert mouse coords from scaled canvas to actual canvas width
-  let xCentreScaled = convertRange2(xCentre, 0, 0, scaledGridWidth, gridWidth);
-  let yCentreScaled = convertRange2(
-    yCentre,
-    0,
-    0,
-    scaledGridHeight,
-    gridHeight
-  );
+  let xCentreScaled = convertRange(xCentre, 0, 0, scaledGridWidth, gridWidth);
+  let yCentreScaled = convertRange(yCentre, 0, 0, scaledGridHeight, gridHeight);
   // In mandelbrot space, coords of the mouse pre zoom
-  const oldXCentreScaled = convertRange2(
+  const oldXCentreScaled = convertRange(
     xCentreScaled,
     0,
     xMin,
     gridWidth,
     xMax
   );
-  const oldYCentreScaled = convertRange2(
+  const oldYCentreScaled = convertRange(
     yCentreScaled,
     0,
     yMax,
@@ -245,14 +212,14 @@ function zoomIn(event) {
   yMax = yMax - yRangeDistance / zoomFactor;
 
   // Coords of the mouse post zoom
-  const newXCentreScaled = convertRange2(
+  const newXCentreScaled = convertRange(
     xCentreScaled,
     0,
     xMin,
     gridWidth,
     xMax
   );
-  const newYCentreScaled = convertRange2(
+  const newYCentreScaled = convertRange(
     yCentreScaled,
     0,
     yMax,
@@ -266,7 +233,7 @@ function zoomIn(event) {
   xMax -= xDifference;
   yMin -= yDifference;
   yMax -= yDifference;
-  drawGraph2();
+  drawGraph();
 }
 
 function zoomOut(event) {
@@ -274,14 +241,8 @@ function zoomOut(event) {
   const xCentre = event.offsetX;
   const yCentre = event.offsetY;
   // Convert mouse coords from scaled canvas to actual canvas width
-  const xCentreScaled = convertRange2(
-    xCentre,
-    0,
-    0,
-    scaledGridWidth,
-    gridWidth
-  );
-  const yCentreScaled = convertRange2(
+  const xCentreScaled = convertRange(xCentre, 0, 0, scaledGridWidth, gridWidth);
+  const yCentreScaled = convertRange(
     yCentre,
     0,
     0,
@@ -289,14 +250,14 @@ function zoomOut(event) {
     gridHeight
   );
   // In mandelbrot space, coords of the mouse pre zoom
-  const oldXCentreScaled = convertRange2(
+  const oldXCentreScaled = convertRange(
     xCentreScaled,
     0,
     xMin,
     gridWidth,
     xMax
   );
-  const oldYCentreScaled = convertRange2(
+  const oldYCentreScaled = convertRange(
     yCentreScaled,
     0,
     yMax,
@@ -312,15 +273,15 @@ function zoomOut(event) {
   yMin = yMin - yRangeDistance / zoomFactor;
   yMax = yMax + yRangeDistance / zoomFactor;
 
-  // // Coords of the mouse post zoom
-  const newXCentreScaled = convertRange2(
+  // Coords of the mouse post zoom
+  const newXCentreScaled = convertRange(
     xCentreScaled,
     0,
     xMin,
     gridWidth,
     xMax
   );
-  const newYCentreScaled = convertRange2(
+  const newYCentreScaled = convertRange(
     yCentreScaled,
     0,
     yMax,
@@ -334,75 +295,13 @@ function zoomOut(event) {
   xMax -= xDifference;
   yMin -= yDifference;
   yMax -= yDifference;
-  drawGraph2();
+  drawGraph();
 }
 
 // x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
 // y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
 
 // NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
-function convertRange(value) {
-  const newValue = [];
-  newValue.push(((value[0] - 0) * (xMax - xMin)) / (gridWidth - 0) + xMin);
-  newValue.push(((value[1] - 0) * (yMax - yMin)) / (gridHeight - 0) + yMin);
-  return newValue;
-}
-
-function convertRange2(value, oldMin, newMin, oldMax, newMax) {
+function convertRange(value, oldMin, newMin, oldMax, newMax) {
   return ((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin;
-}
-
-// console.log(canvas2);
-// console.log(multiplyMatrix());
-
-function drawGraph() {
-  return;
-  // return;
-  // Clear canvas
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < gridWidth; i += inteval) {
-    for (let j = 0; j < gridHeight; j += inteval) {
-      const c = convertRange([i, j]);
-      let x = 0;
-      let y = 0;
-      let iteration = 0;
-      let breakBounds = false;
-      while (iteration < maxIteration) {
-        if (x * x + y * y > 2 * 2) {
-          breakBounds = true;
-          break;
-        }
-        // zn = zn-1 + c
-        // (a + b) (c + d) = (ac - bd) + (ad + bc)
-        const tempX = x ** 2 - y ** 2 + c[0];
-        y = 2 * x * y + c[1];
-        x = tempX;
-        iteration++;
-      }
-      if (breakBounds) {
-        // console.log(iteration);
-        // const rgb = scale(iteration);
-        // context.fillStyle = `rgb(${rgb[0]} ${rgb[1]} ${rgb[2]})`;
-        const brightness = scale(iteration);
-        // console.log(brightness);
-        context.fillStyle = `rgb(0,0,255,${brightness})`;
-        // context.fillStyle = "red";
-        context.fillRect(i, gridHeight - j, 1, 1);
-      } else {
-        context.fillStyle = "black";
-        context.fillRect(i, gridHeight - j, 1, 1);
-      }
-    }
-  }
-}
-
-function scale(x) {
-  // // https://stackoverflow.com/questions/39059921/how-can-i-convert-a-number-scale-into-color-lightness-using-javascript
-  // return [
-  //   Math.round(0xff * (1 - x / maxIteration)),
-  //   0,
-  //   Math.round(0xff * (x / maxIteration)),
-  // ];
-  // scale x from 0 - maxIteration into 0 - 1
-  return convertRange2(x, 0, 0, maxIteration, 1);
 }
