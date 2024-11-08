70a6144cd3d7b471746ce3687aa7b8792825b396
joshuaWithey <83647960+joshuaWithey@users.noreply.github.com>
2024-11-08 18:36:57 +1100

    temp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let canvas = document.getElementById("myCanvas");
 const windowHeight = window.innerHeight;
 
 // Render width of the canvas
-const gridWidth = 1000;
+const gridWidth = 500;
 const aspectRatio = window.innerHeight / window.innerWidth;
 
 let mouseDownX;
@@ -13,7 +13,8 @@ let mouseDown = false;
 const gridHeight = Math.round(gridWidth * aspectRatio);
 canvas.setAttribute("width", gridWidth);
 canvas.setAttribute("height", gridHeight);
-const gl = canvas.getContext("webgl2", { premultipliedAlpha: false });
+// const gl = canvas.getContext("webgl2", { premultipliedAlpha: false });
+const ctx = canvas.getContext("2d");
 const scaledGridWidth = canvas.offsetWidth;
 const scaledGridHeight = canvas.offsetHeight;
 const inteval = 1;
@@ -56,7 +57,7 @@ for (let i = 0; i < gridHeight; i++) {
 
 const gpu = new window.GPU.GPU({
   canvas,
-  context: gl,
+  // context: gl,
 });
 const createCanvas = gpu.createKernel(
   function (xArray, yArray, xMin, xMax, yMin, yMax) {
@@ -97,20 +98,52 @@ const createCanvas = gpu.createKernel(
       gridHeight: gridHeight,
       maxIteration: maxIteration,
     },
-    functions: {
-      convertRange,
-    },
     graphical: true,
   }
 );
+function CPUCreateCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < gridWidth; i++) {
+    for (let j = 0; j < gridHeight; j++) {
+      const oldX = (i * (xMax - xMin)) / (gridWidth - 1) + xMin;
+      const oldY = (j * (yMax - yMin)) / (gridHeight - 1) + yMin;
+      let x = 0;
+      let y = 0;
+      let iteration = 0;
+      let breakBounds = false;
+      while (iteration < maxIteration) {
+        if (x * x + y * y > 2 * 2) {
+          breakBounds = true;
+          break;
+        }
+        // zn = zn-1 + c
+        // (a + b) (c + d) = (ac - bd) + (ad + bc)
+        const tempX = x ** 2 - y ** 2 + oldX;
+        y = 2 * x * y + oldY;
+        x = tempX;
+        iteration++;
+      }
+      if (breakBounds) {
+        const brightness = Math.sqrt(iteration / maxIteration);
+        ctx.fillStyle = `rgba(0,0,255,brightness)`;
+        ctx.fillRect(i, gridHeight - j, 1, 1);
+        // this.color(0, 0, 1, brightness);
+      } else {
+        // ctx.fillRect(0, 0, 100, 100);
+        // this.color(0, 0, 0, 1);
+      }
+    }
+  }
+}
 function drawGraph() {
-  createCanvas(xArray, yArray, xMin, xMax, yMin, yMax);
-  canvas = document.getElementById("myCanvas");
-  const newCanvas = createCanvas.canvas;
-  newCanvas.id = "myCanvas";
-  newCanvas.style = "width: 100%";
-  canvas.parentNode.replaceChild(newCanvas, canvas);
-
+  CPUCreateCanvas();
+  // createCanvas(xArray, yArray, xMin, xMax, yMin, yMax);
+  // canvas = document.getElementById("myCanvas");
+  // const newCanvas = createCanvas.canvas;
+  // newCanvas.id = "myCanvas";
+  // newCanvas.style = "width: 100%";
+  // canvas.parentNode.replaceChild(newCanvas, canvas);
+  const newCanvas = canvas;
   // Update event listeners
   newCanvas.addEventListener("wheel", scrollEvent);
   newCanvas.addEventListener("mousemove", mousePan);
